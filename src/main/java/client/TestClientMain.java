ac117ae658461eaa93b74662ce8bb85dcdcd75e9
Falco Winkler <falcomail95@yahoo.de>
2017-06-20 11:29:58 +0000

    made host & port configurable

--- src/main/java/client/TestClientMain.java
+++ src/main/java/client/TestClientMain.java
@@ -6,7 +6,15 @@ import mware_lib.ObjectBroker;
 
 public class TestClientMain {
     public static void main(String[] args) {
-        ObjectBroker objectBroker = ObjectBroker.init("localhost", 8080, true);
+
+        if(args.length != 2) {
+            System.out.println("Usage TestClientMain <nameservice-host> <nameservice-port>");
+            return;
+        }
+        String host = args[0];
+        int port = Integer.valueOf(args[1]);
+
+        ObjectBroker objectBroker = ObjectBroker.init(host, port, true);
         NameService nameService = objectBroker.getNameService();
         Object rawRef = nameService.resolve("zumsel");
         _CalculatorImplBase implBase = _CalculatorImplBase.narrowCast(rawRef);
--- src/main/java/server/TestServerMain.java
+++ src/main/java/server/TestServerMain.java
@@ -6,9 +6,18 @@ import mware_lib.ObjectBroker;
 
 public class TestServerMain {
     public static void main(String[] args) {
-        ObjectBroker objectBroker = ObjectBroker.init("localhost", 8080, false);
+
+        if(args.length != 2) {
+            System.out.println("Usage TestClientMain <nameservice-host> <nameservice-port>");
+            return;
+        }
+        String host = args[0];
+        int port = Integer.valueOf(args[1]);
+
+        ObjectBroker objectBroker = ObjectBroker.init(host, port, false);
         NameService nameService = objectBroker.getNameService();
         Calculator calculator = new Calculator();
         nameService.rebind(calculator, "zumsel");
+        // TODO: start receiver here
     }
 }
